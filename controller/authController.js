0ce2ba61cac4e09c3d59cd2a333389f79b2ac2fb
FiorenNathasia <fiorennathasia@gmail.com>
2025-01-29 21:34:17 +0000

    Made user table

---
+++ controller/authController.js
@@ -0,0 +1,10 @@
+const db = require("../db/db");
+
+const login = async (req, res) => {};
+
+const signup = async (req, res) => {};
+
+module.exports = {
+  login,
+  signup,
+};
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
+const authRouter = require("./routes/auth");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/auth", authRouter);
 
 app.listen(8181, () => {
   console.log("server listening on 8181");
