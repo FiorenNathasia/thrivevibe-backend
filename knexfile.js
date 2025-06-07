1986357c46c3e6bdcc38e27efa255cbe0e101844
FiorenNathasia <fiorennathasia@gmail.com>
2025-06-07 21:56:09 +0100

    Deploy

--- knexfile.js
+++ knexfile.js
@@ -18,4 +18,14 @@ module.exports = {
       tableName: "knex_migrations",
     },
   },
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+  },
 };
