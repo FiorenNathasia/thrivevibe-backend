53a9d44bc39a0e571d4dc963133e25126a63f390
FiorenNathasia <fiorennathasia@gmail.com>
2025-07-16 22:40:32 +0100

    Summary router fix

--- routes/videos.js
+++ routes/videos.js
@@ -4,10 +4,11 @@ const videosController = require("../controller/videosController");
 
 router.post("/newvideo", videosController.newVideo);
 router.get("/", videosController.getVideos);
+router.get("/:id/summary", videosController.getVideoSummary);
 router.get("/:id", videosController.getVideo);
 router.put("/:id/upvote", videosController.updateUpvote);
 router.put("/:id/downvote", videosController.updateDownvote);
 router.patch("/:id", videosController.editVideo);
 router.delete("/:id", videosController.deleteVideo);
-router.get("/:id/summary", videosController.getVideoSummary);
+
 module.exports = router;
